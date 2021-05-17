7a397ec2c372d37d8c87763889085dc95f063beb
Machiko Takahashi <machiko.ihia22@gmail.com>
2021-05-17 16:07:09 +0900

    continue

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,18 @@
 class QuestionsController < ApplicationController
-  def ask
 
+  def ask
+  end
+
+  def answer
+    @question = params['question']
+
+    # if @question =~ /i am going to work/i
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
